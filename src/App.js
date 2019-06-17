d556cc8668d94cc5e0d150b24e484b864e1fb22a
Chris Knight Johnson <cknightjohnson@gmail.com>
2019-06-17 10:27:28 -0700

    add post editing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Button, Container } from 'reactstrap';
 
 import PostViewer from './PostViewer';
+import PostEditor from './PostEditor';
 
-function App() {
-  return (
-    <main>
-      <PostViewer />
-    </main>
-  );
+class App extends Component {
+  state = {
+    editing: null,
+  };
+
+  render() {
+    const { editing } = this.state;
+
+    return (
+      <Container fluid>
+        <Button
+          className="my-2"
+          color="primary"
+          onClick={() => this.setState({ editing: {} })}
+        >
+          New Post
+        </Button>
+        <PostViewer
+          canEdit={() => true}
+          onEdit={(post) => this.setState({ editing: post })}
+        />
+        {editing && (
+          <PostEditor
+            post={editing}
+            onClose={() => this.setState({ editing: null })}
+          />
+        )}
+      </Container>
+    );
+  }
 }
 
 export default App;
